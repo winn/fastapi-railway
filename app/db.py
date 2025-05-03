e5fa0ad96d08a6ef9ed415f02c266af41e9ba026
winn voravuthikunchai <vwinnv@winns-MacBook-Air.local>
2025-05-03 21:39:20 +0700

    Initial FastAPI app with MongoDB

--- app/db.py
+++ app/db.py
@@ -1,14 +1,14 @@
-# import os
-# from motor.motor_asyncio import AsyncIOMotorClient
-# from dotenv import load_dotenv
 import os
-from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+# import os
+# from pymongo import MongoClient
 
 
 # load_dotenv()
 
-mongo_uri = os.getenv("MONGO_URL")
-client = MongoClient(mongo_uri)
+MONGO_URI = os.getenv("MONGO_URI")
+client = AsyncIOMotorClient(MONGO_URI)
 db = client["railway_db"]
 collection = db["items"]
 
